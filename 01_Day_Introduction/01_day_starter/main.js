eb9ac4b99c691cb80562cfb1dd831ed0d8817e04
anupama12sg <anupama12sg@gmail.com>
2022-11-16 00:57:05 +0530

    My very first work at JS

--- 01_Day_Introduction/01_day_starter/main.js
+++ 01_Day_Introduction/01_day_starter/main.js
@@ -1,4 +1,6 @@
 // the variable values can be accessed from other variable.js file
 console.log(firstName, lastName, country, city, age, isMarried) 
-console.log(gravity, boilingPoint, PI) // 9.81, 100, 3.14
+console.log(`my first Name is ${firstName} \n my lastName  is ${lastName} `)
+console.log('my firstName is ', firstName)
+console.log(gravity, boilingPoint, PI) 
 console.log(name, job, live)
